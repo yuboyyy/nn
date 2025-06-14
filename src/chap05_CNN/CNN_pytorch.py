9eaff56be0e46e3ba4e72d9f41f8a0bce034758b
11lijing <2477813589@qq.com>
2025-06-14 20:00:35 +0800

    增加超参数解释 (#2252)
    * 增加注释4
    
    * 解释超参数

--- src/chap05_CNN/CNN_pytorch.py
+++ src/chap05_CNN/CNN_pytorch.py
@@ -26,7 +26,8 @@ import torch.utils.data as Data
 # 导入 torchvision 库，它包含了常用的计算机视觉数据集、模型结构和图像处理工具
 import torchvision
 
-# 设置超参数
+# 设置超参数。超参数（Hyperparameters）是机器学习模型在训练前需要手动设定（或通过算法优化）的配置参数，
+# 它们不直接从数据中学习，而是控制模型的整体行为和性能。
 learning_rate = 1e-4  #  学习率：控制参数更新步长
 keep_prob_rate = 0.7  #  Dropout保留神经元的比例：防止过拟合
 max_epoch = 3         # 训练的总轮数

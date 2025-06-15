2163508f3f368b217542db9cdfa9a01d6179f129
pyy12l38 <2057798697@qq.com>
2025-06-15 10:31:57 +0800

    Update tutorial_mnist_conv-keras.py

--- src/chap05_CNN/tutorial_mnist_conv-keras.py
+++ src/chap05_CNN/tutorial_mnist_conv-keras.py
@@ -5,6 +5,7 @@
 # In[1]:
 import os
 import tensorflow as tf
+# 从TensorFlow中导入Keras高级API（TensorFlow的内置Keras实现）
 from tensorflow import keras
 from tensorflow.keras import layers, optimizers, datasets # 导入Keras核心组件：层定义、优化器和常用数据集
 from tensorflow.keras.layers import ( 

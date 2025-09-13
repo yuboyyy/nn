2ed0db2bc5f857d463adc83bcdaed48325d7e70e
aasdar <3415807009@qq.com>
2025-09-13 18:14:13 +0800

    为语句引入变量名 (#2501)
    1.为比较自定义的softmax函数结果和tf自带的结果添加变量名，有利于后续的调用
    2.为比较自定义的sigmoid函数结果和tf自带的结果添加变量名，有利于后续的调用

--- src/chap04_simple_neural_network/tf2.0-exercise.py
+++ src/chap04_simple_neural_network/tf2.0-exercise.py
@@ -43,13 +43,13 @@ def softmax(x: tf.Tensor) -> tf.Tensor:
 # 生成测试数据，形状为 [10, 5] 的正态分布随机数
 test_data = np.random.normal(size = [10, 5])
 # 比较自定义的softmax函数结果和tf自带的结果，误差小于 0.0001 则认为相等
-(softmax(test_data).numpy() - tf.nn.softmax(test_data, axis = -1).numpy())**2 < 0.0001
+var1 = (softmax(test_data).numpy() - tf.nn.softmax(test_data, axis=-1).numpy()) ** 2 < 0.0001
 
 # 数值稳定的 Softmax 函数，用于将原始预测值（logits）转换为概率分布
 
 def sigmoid(x):
     ##########
-    '''实现sigmoid函数， 不允许用tf自带的sigmoid函数'''
+    """实现sigmoid函数， 不允许用tf自带的sigmoid函数"""
     # 将输入x转换为float32类型，确保数值计算的精度和类型一致性。
     x = tf.cast(x, tf.float32)
     # sigmoid 数学定义：1 / (1 + e^{-x})
@@ -58,13 +58,13 @@ def sigmoid(x):
 # 生成测试数据，形状为 [10, 5] 的正态分布随机数
 test_data = np.random.normal(size = [10, 5])
 # 比较自定义的sigmoid函数结果和tf自带的结果，误差小于 0.0001 则认为相等
-(sigmoid(test_data).numpy() - tf.nn.sigmoid(test_data).numpy())**2 < 0.0001
+var2 = (sigmoid(test_data).numpy() - tf.nn.sigmoid(test_data).numpy()) ** 2 < 0.0001
 
 # ## 实现 softmax 交叉熵loss函数
 
 def softmax_ce(logits, label):
     ##########
-    '''实现 softmax 交叉熵loss函数， 不允许用tf自带的softmax_cross_entropy函数'''
+    """实现 softmax 交叉熵loss函数， 不允许用tf自带的softmax_cross_entropy函数"""
     logits = tf.cast(logits, tf.float32)
     label = tf.cast(label, tf.float32)
     # 参数logits: 未经Softmax的原始输出（logits）
